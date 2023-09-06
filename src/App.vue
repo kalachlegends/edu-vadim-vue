2644a26b82106651f8f8aa1a57e585437569fa90
kalachlegends <artem.kalachev.2002@gmail.com>
2023-09-06 17:38:16 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import postVue from "./components/post.vue";
+
+export default {
+  data: () => ({ count: 0 }),
+  components: { postVue },
+  methods: {
+    handleCLick: function (e) {
+      console.log(this);
+    },
+    handleCLickTwo: function (e) {
+      console.log(this.count++);
+    },
+  },
+};
+</script>
+
+<template>
+  <postVue v-if="count > 2" :title="count" discription="'2'" />
+  <div>Я появился</div>
+  <div @click="handleCLick">Я event handleCLick</div>
+  <div @click="handleCLickTwo">Я event handleCLickTwo</div>
+  <!-- HINTS for 1: <img src="/vite.svg" /> -->
+</template>
+
+<style scoped></style>
---
+++ src/components/post.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="post">
+    <div class="post__title">{{ title }}</div>
+    <div class="post__description">{{ discription }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    discription: String,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.post {
+  padding: 10px;
+  background: white;
+  color: black;
+  &__title {
+    font-weight: bold;
+    font-size: 20px;
+  }
+  &__description {
+    font-weight: normal;
+    font-size: 15px;
+  }
+}
+</style>
